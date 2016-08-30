73b651fe65b8cd0ba304961d07041c7ae1dd698d
shirleyR <shuilinren@foxmail.com>
2016-08-30 22:46:21 +0800

    change doPost
    service should be hind

--- webServer/src/com/server/SnoopServlet.java
+++ webServer/src/com/server/SnoopServlet.java
@@ -3,7 +3,7 @@ package com.server;
 
 public class SnoopServlet extends Servlet{
 	@Override
-	public void service(HttpRequest request,HttpResponse response){
+	public void doPost(HttpRequest request,HttpResponse response){
 		System.out.println("Servlet");
 		System.out.println(request.getAction());
 	}
